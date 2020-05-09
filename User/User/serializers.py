a167ad74709c77570fc9a53f0ce4c8730d5fdfae
Janis <janisgoesser92@gmail.com>
2020-05-09 14:38:57 +0200

    Create basic User Serializer for registration

--- User/User/serializers.py
+++ User/User/serializers.py
@@ -1,1 +1,18 @@
 from rest_framework import serializers
+
+from .models import User
+
+class BaseUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+        extra_kwargs = {
+            'password':{
+                'write_only':True,
+            },
+        }
+
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
+
