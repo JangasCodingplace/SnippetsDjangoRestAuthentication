aadfab4a0957bd9acdb84a908decd8a0037d5ac0
Janis <janisgoesser92@gmail.com>
2020-05-09 15:05:52 +0200

    Include .env and .env settings to Software

--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-import os
 import sys
+import env_file
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_config.settings')
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', '_config.settings')
+    env_file.load()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
